3172bb40dc1e294a8f0cc67dfe5479b7d6621d07
_GGDol <bohg6799@naver.com>
2025-03-28 16:50:20 +0900

    feat: Lv 1 구현
    - Lv 1 구현

---
+++ HarryPotterSeriesApp/Home/Series/SeriesView.swift
@@ -0,0 +1,48 @@
+//
+//  Series.swift
+//  HarryPotterSeriesApp
+//
+//  Created by 양원식 on 3/28/25.
+//
+
+import UIKit
+import SnapKit
+
+class SeriesButtonView: UIView {
+    
+    // 시리즈 버튼 [ 추후 시리즈 숫자에 맞게 버튼 생성 ]
+    private let button: UIButton = {
+        var button = UIButton()
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 15
+        button.clipsToBounds = true
+        return button
+    }()
+    
+    // MARK: - 초기화
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - UI 구성
+    private func setupUI() {
+        addSubview(button)
+        button.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.width.height.equalTo(30)
+            $0.top.equalToSuperview().offset(16)
+            $0.bottom.equalToSuperview()
+        }
+    }
+    
+    func configure(with title: String) {
+        button.setTitle(title, for: .normal)
+    }
+}
---
+++ HarryPotterSeriesApp/Home/Title/TitleView.swift
@@ -0,0 +1,45 @@
+//
+//  TitleView.swift
+//  HarryPotterSeriesApp
+//
+//  Created by 양원식 on 3/28/25.
+//
+
+import UIKit
+import SnapKit
+
+class TitleView: UIView {
+    
+    // 타이틀
+    private let titleLabel: UILabel = {
+        var label = UILabel()
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.numberOfLines = 2
+        label.lineBreakMode = .byWordWrapping
+        return label
+    }()
+    
+    // MARK: - 초기화
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - UI 구성
+    private func setupUI() {
+        addSubview(titleLabel)
+        titleLabel.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.top.bottom.equalToSuperview()
+        }
+    }
+    
+    func configure(with title: String) {
+        titleLabel.text = title
+    }
+}
