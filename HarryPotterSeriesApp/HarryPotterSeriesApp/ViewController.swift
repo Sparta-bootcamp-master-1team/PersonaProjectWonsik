5c8928ee65dc4ed471f22eaee9f880249aedd252
_GGDol <bohg6799@naver.com>
2025-03-31 18:13:13 +0900

    Revert "Feature/#5 LV 3 구현"

---
+++ HarryPotterSeriesApp/HarryPotterSeriesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HarryPotterSeriesApp
+//
+//  Created by 양원식 on 3/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HarryPotterSeriesApp/HarryPotterSeriesAppTests/HarryPotterSeriesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterSeriesAppTests.swift
+//  HarryPotterSeriesAppTests
+//
+//  Created by 양원식 on 3/28/25.
+//
+
+import Testing
+@testable import HarryPotterSeriesApp
+
+struct HarryPotterSeriesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
